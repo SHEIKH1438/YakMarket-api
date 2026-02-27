0c3638bfde629b53c5ef92709f6085e7e865e18a
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-28 00:25:08 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -7,21 +7,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.gmail.com'),
-        port: env.int('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-      },
-      settings: {
-        defaultFrom: 'noreply@yakmarket.tj',
-        defaultReplyTo: 'support@yakmarket.tj',
-      },
-    },
-  },
 });
