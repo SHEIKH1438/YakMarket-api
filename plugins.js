79f0cc60c089e970fafb05727b5bd9d5c353fe62
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-27 22:36:47 +0500

    Add files via upload

--- plugins.js
+++ plugins.js
@@ -1,49 +1,11 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('ADMIN_JWT_SECRET'),
-      register: {
-        roles: ['Public'],
-        default: 'Authenticated',
-      },
-    },
-  },
-
-  // Email
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.gmail.com'),
-        port: env.int('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-      },
-    },
-  },
-
-  // Cloudinary
   upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
         cloudinary_url: env('CLOUDINARY_URL'),
       },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
     },
   },
-
-  // I18n - ТУТ БЫЛА ОШИБКА, ИСПРАВИЛ
-  i18n: {
-    enabled: true,
-    config: {
-      defaultLocale: 'ru',
-      locales: ['ru', 'tg', 'en'],
-    },
-  },
+  // Мы полностью убрали i18n, чтобы он не вешал сервер
 });
