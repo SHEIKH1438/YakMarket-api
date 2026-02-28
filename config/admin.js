1a34db79563d7ea7bd9571803693b39d49ea6933
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-28 11:47:01 +0500

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,15 +1,13 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
-    },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
   },
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloudinary_url: env('CLOUDINARY_URL'),
-      },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
 });
