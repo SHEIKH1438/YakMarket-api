e3fa50de0db1b293f9e2f5380237080f69115406
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-28 17:51:23 +0500

    Add content security policy configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,10 +3,26 @@ module.exports = [
   {
     name: 'strapi::security',
     config: {
-      contentSecurityPolicy: false,
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // Разрешаем запросы со всех адресов, включая твой 127.0.0.1
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
