ccf93170c6c60fee70af4a0bd5e229b6a75008c4
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-28 13:03:52 +0500

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,23 +3,7 @@ module.exports = [
   {
     name: 'strapi::security',
     config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:', 'wss:', 'ws:'],
-          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https:', 'http:'],
-          'style-src': ["'self'", "'unsafe-inline'", 'https:', 'http:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'https:', 'http:'],
-          'media-src': ["'self'", 'res.cloudinary.com', 'https:', 'http:'],
-          'upgrade-insecure-requests': null,
-        },
-      },
-      cors: {
-        origin: ['http://localhost:3000', 'http://localhost:8081', 'https://yakmarket.tj', 'https://api.yakmarket.tj'],
-        credentials: true,
-        headers: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'Accept'],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      },
+      contentSecurityPolicy: false,
     },
   },
   'strapi::cors',
@@ -30,6 +14,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'global::csrf',
-  'global::isOwner',
 ];
