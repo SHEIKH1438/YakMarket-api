b05d896d6fa7e3106da5ad216f3c2f0270a9423d
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-02-28 11:40:24 +0500

    Add admin configuration for users and upload settings

---
+++ config/admin.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloudinary_url: env('CLOUDINARY_URL'),
+      },
+    },
+  },
+});
