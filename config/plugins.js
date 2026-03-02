16bfd01a7bc9ec02bd30834d130271748b0499a6
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-03-02 15:41:08 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -3,12 +3,16 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('CLOUDINARY_NAME', 'dgdpxe2fq'),
+        api_key: env('CLOUDINARY_KEY', 'axJ_0xQcmr4kPrXinuVJ2REhs3o'),
+        api_secret: env('CLOUDINARY_SECRET', '437375988755669'),
       },
       actionOptions: {
-        upload: {},
+        upload: {
+          allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'svg'],
+          resource_type: 'image',
+          access_mode: 'public',
+        },
         delete: {},
       },
     },
@@ -23,6 +27,13 @@ module.exports = ({ env }) => ({
         defaultFrom: env('EMAIL_FROM'),
         defaultReplyTo: env('EMAIL_FROM'),
       },
+    },
+  },
+  // Google Auth is mostly configured in the Admin Panel (Settings -> Providers),
+  // but we can ensure the users-permissions plugin is ready.
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'yakmarket-jwt-secret-2024'),
     },
   },
 });
