ecba8d79fe806cafb7595a2f85d04c342b076e4b
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-03-02 15:39:55 +0500

    Set default values for admin JWT and token salts
    Updated environment variable defaults for admin configuration.

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+    auth: {
+        secret: env('ADMIN_JWT_SECRET', 'yakmarket-admin-jwt-secret-2024'),
     },
-  },
+    apiToken: {
+        salt: env('API_TOKEN_SALT', 'yakmarket-api-token-salt-2024'),
+    },
+    transfer: {
+        token: {
+            salt: env('TRANSFER_TOKEN_SALT', 'yakmarket-transfer-token-salt-2024'),
+        },
+    },
 });
