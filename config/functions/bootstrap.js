3a8d1005db97d7d94df95090fa142b03cf7a5739
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-03-02 16:02:58 +0500

    Update bootstrap.js

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -2,8 +2,8 @@
 const axios = require('axios');
 
 module.exports = async ({ strapi }) => {
-    const botToken = process.env.TELEGRAM_TOKEN;
-    
+    const botToken = process.env.TELEGRAM_BOT_TOKEN || '8662410817:AAEPg37YkiJ6XnfnpmDW_fg1kp0hsz2_Eh0';
+
     // Прямой массив данных, чтобы точно сработало без поиска файлов
     const moderators = [
         { id: '8012802187', name: 'SheikhK2' }
@@ -11,7 +11,7 @@ module.exports = async ({ strapi }) => {
 
     for (const mod of moderators) {
         try {
-            await axios.post(`https://api.telegram.org{botToken}/sendMessage`, {
+            await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
                 chat_id: mod.id,
                 text: `🚀 <b>YakMarket Bot запущен!</b>\n\nПоздравляем, <b>${mod.name}</b>, система модерации активна 24/7.\n\nЖду новых объявлений!`,
                 parse_mode: 'HTML'
