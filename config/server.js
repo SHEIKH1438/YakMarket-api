04832cbf22ca45348050773e8eb604b5d4be5aa9
SHEIKH1438 <muhajeodinaeva7@gmail.com>
2026-03-02 15:41:24 +0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('URL'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('URL', 'https://yakmarket-api-production.up.railway.app'),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
 });
